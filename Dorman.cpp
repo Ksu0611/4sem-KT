3b599b7e7450529878af3e84aa05568b9b235ccb
Ксения Шпилевская <100766019+Ksu0611@users.noreply.github.com>
2023-03-31 13:07:00 +0300

    Update Dorman.cpp

--- Dorman.cpp
+++ Dorman.cpp
@@ -1,10 +1,4 @@
-//============================================================================
-// Name        : Dorman.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+
 
 #include <iostream>
 #include <fstream>
